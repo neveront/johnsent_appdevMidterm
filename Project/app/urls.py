588fa248a86dfeb9029d3e21279f46eddbe50e10
neveront <memberty.asis@gmail.com>
2025-11-16 00:54:49 +0800

    added 'create function'

--- Project/app/urls.py
+++ Project/app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import HomePageView
+from .views import HomePageView, PostCreateView
 
 urlpatterns = [
-    path('',HomePageView.as_view(), name='home'),
+    path('', HomePageView.as_view(), name='home'),
+    path('post/create/', PostCreateView.as_view(), name='post-create'),
 ]
--- Project/app/views.py
+++ Project/app/views.py
@@ -1,8 +1,44 @@
-from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import PostForm
+from .models import Post
+from django.shortcuts import render, redirect
+from .models import Post
+from .forms import PostForm  # assuming you have a ModelForm
+from django.views.generic import ListView
 
-class HomePageView(TemplateView):
-    template_name = 'app/home.html'
+class HomePageView(ListView):
+    model = Post
+    template_name = 'app/home.html'  # your template that shows all posts
+    context_object_name = 'posts'  # this variable is used in template
 
-    # def get(self, request, *args, **kwargs):
-    #     return HttpResponse("<h1>Hello World from view!</h1>")
+def post_list(request):
+    posts = Post.objects.all()  # fetch all posts
+    return render(request, 'app/home.html', {'posts': posts})
+
+
+
+class PostCreateView(View):
+    template_name = 'app/post_create.html'
+
+    def get(self, request):
+        form = PostForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')  # redirect to home after creation
+        return render(request, self.template_name, {'form': form})
+    
+    # def post_create(request):
+    #     if request.method == 'POST':
+    #         form = PostForm(request.POST)
+    #         if form.is_valid():
+    #             form.save()
+    #             return redirect('post_list')  # make sure this matches your URL name
+    #     else:
+    #         form = PostForm()
+    #     return render(request, 'post_create.html', {'form': form})
